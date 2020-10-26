4355d763b7cb3aaab973ea7af3033adf5413c072
DukeWilki <65857753+DukeWilki@users.noreply.github.com>
2020-10-26 20:01:10 +1100

    test commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// Veiw tables
+// Make Res button /reserve /home /tables
+
+// make ressies
+
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3001;
+
+const tables = [
+    {
+        id: "",
+        name: "",
+        email: "",
+        phone: "",
+        pax: ""
+      }
+];
+const waitlist = [];
+
+// HTML
+app.get('/')
+app.get('/reservations/new')
+app.get('/reservations/view')
+
+// API
+app.get('/api/waitlist/all')
+app.get('/api/reservations/all')
+app.post('/api/reservations/create')
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+  // Basic route that sends the user first to the AJAX Page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join("/reservations", "/home.html"));
+  });
+
+  app.get("/reservations/new", function(req, res) {
+    res.sendFile(path.join("/reservations", "/new.html"));
+  });
+
+  app.get("/reservations/view", function(req, res) {
+    res.sendFile(path.join("/reservations", "/view.html"));
+  });
+
+
+
+  app.listen(PORT, function() {
+      console.log("Listening on " + PORT);
+  })
+  
